49ec08e0e2e35a75ee90d4cf7a0a252c4bfe6bcf
lakhan_24 <fakelakhan600@gmail.com>
2023-07-20 16:58:28 +0530

    Mark two

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 import { Container, Row, Button} from 'reactstrap'
 import logo from '../../assets/image/travel tales.jpg'
 import { NavLink, Link } from 'react-router-dom'
@@ -20,7 +20,29 @@ import './header.css'
  ]
 
 const Header = () => { 
-  return <header className="header">
+
+  const headerRef = useRef(null)
+
+  const stickyHeaderFunc=()=>{
+    window.addEventListener('scroll',()=>{
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop>80){
+        headerRef.current.classList.add('sticky__header')
+
+      }
+      else{
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+
+useEffect(()=>{
+  stickyHeaderFunc()
+  return window.removeEventListener('scroll',stickyHeaderFunc);
+})
+
+
+  return <header className="header" ref={headerRef}>
     <Container>
       <Row>
         <div className="nav__wrapper d-flex align-items-center justify-content-between">
